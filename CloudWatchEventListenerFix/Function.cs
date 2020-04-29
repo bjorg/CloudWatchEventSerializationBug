8d48ae2d9d899ab6c31547e59de4f00beb360371
Steve Bjorg <steve.bjorg@gmail.com>
2020-04-29 09:38:50 -0700

    initial commit

---
+++ CloudWatchEventListenerFix/Function.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using Amazon.Lambda.CloudWatchEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Amazon.Lambda.CloudWatchEvents {
+
+    public class MyCloudWatchEvent : CloudWatchEvent<CloudWatchEventListener.EventDetails> { }
+}
+
+namespace CloudWatchEventListener {
+
+    public class EventDetails {
+
+        //--- Properties ---
+        public string Message { get; set; }
+    }
+
+    public class Function {
+
+        //--- Methods ---
+        public string FunctionHandler(MyCloudWatchEvent request, ILambdaContext context) {
+            Console.WriteLine($"Version = {request.Version}");
+            Console.WriteLine($"Account = {request.Account}");
+            Console.WriteLine($"Region = {request.Region}");
+            Console.WriteLine($"Detail = {JsonConvert.SerializeObject(request.Detail)}");
+            Console.WriteLine($"DetailType = {request.DetailType}");
+            Console.WriteLine($"Source = {request.Source}");
+            Console.WriteLine($"Time = {request.Time}");
+            Console.WriteLine($"Id = {request.Id}");
+            Console.WriteLine($"Resources = [{string.Join(",", request.Resources ?? Enumerable.Empty<string>())}]");
+            return "Okay";
+        }
+    }
+}
